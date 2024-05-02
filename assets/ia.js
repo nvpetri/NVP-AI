f5a43f65d31ca0a8b241ef39b02f03260c11662b
nvpetri <23111760@CFP127ALUNO.LOCAL>
2024-05-02 16:52:59 -0300

    translate

--- assets/ia.js
+++ assets/ia.js
@@ -37,8 +37,24 @@ const consultaGemini = (question) => {
 const respostaIa = (responseTextIa) => {
     const textAreaPt = document.getElementById('answer-pt')
     textAreaPt.value = responseTextIa
+
+    consultaMyMemory(responseTextIa, (translatedText) => {
+        const responseEnElement = document.getElementById('answer-pt')
+        responseEnElement.innerText = translatedText
+    })
 }
 
+const consultaMyMemory = (text, callback) => {
+    const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(text)}&langpair=pt|en`
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const translatedText = data.responseData.translatedText
+            callback(translatedText)
+        })
+        .catch(error => console.error('Error: ', error))
+}
 
 button.addEventListener('click', () => {
     const question = document.getElementById('ask--user').value
